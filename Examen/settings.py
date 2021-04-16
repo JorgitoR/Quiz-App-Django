927234ab2bb37d70908864ab29afa05788b4bb76
JorgitoR <jorgitouribe133@gmail.com>
2021-04-16 10:39:27 -0500

    app de quiz

--- Examen/settings.py
+++ Examen/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #aplicaciones de tercero
+    'crispy_forms',
 
-    'Quiz'
+    'Quiz',
 ]
 
 MIDDLEWARE = [
