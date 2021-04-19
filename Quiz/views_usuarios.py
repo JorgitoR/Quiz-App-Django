d21b403dbb15e8dd8abbb86e8776af486c26affc
JorgitoR <jorgitouribe133@gmail.com>
2021-04-19 10:54:56 -0500

    app de quiz

--- Quiz/views_usuarios.py
+++ Quiz/views_usuarios.py
@@ -8,6 +8,9 @@ from django.db import transaction
 from django.db.models import Count
 
 
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 from django.views.generics import CreateView, ListView, UpdateView
 from django.utils.decorators import method_decoratos
 
@@ -30,3 +33,17 @@ class PersonalRegistro(CreateView):
 		usuario = form.save()
 		login(self.request, usuario)
 		return redirect('')
+
+
+class ActualizarInteres(UpdateView):
+	model = ExamenUsuario
+	form_class = CategoriaForms
+	template_name = 'tablero/personal/categoria_form.html'
+
+	def get_object(self):
+		return self.request.user.examenusuario
+
+	def form_valid(self, form):
+		messages.success(self.request, 'Categoria actualizado correctamente')
+		return super().form_valid(form)
+		
